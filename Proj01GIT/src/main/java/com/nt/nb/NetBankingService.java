b6d1494219edae7f700a31c04d33ba04a3850818
sumanta-demo <kumarsumanta083@gmail.com>
2025-05-01 17:37:36 +0530

    TL reviewed NB code

--- Proj01GIT/src/main/java/com/nt/nb/NetBankingService.java
+++ Proj01GIT/src/main/java/com/nt/nb/NetBankingService.java
@@ -1,6 +1,7 @@
 package com.nt.nb;
 
 public class NetBankingService {
+	//By default performs RTGS payment
 	public String transferMoney(long srcAccount,long destAmount,String ifscode,double amt) {
 		return amt+" is transfering from "+srcAccount+" to "+destAmount;
 	}
