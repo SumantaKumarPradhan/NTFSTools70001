abcb4c5225cf6f8dab8788141683038fc8a9b724
sumanta-demo <kumarsumanta083@gmail.com>
2025-05-01 16:11:32 +0530

    dev save

--- Proj01GIT/src/main/java/com/nt/service/Arithmetic.java
+++ Proj01GIT/src/main/java/com/nt/service/Arithmetic.java
@@ -3,7 +3,7 @@ package com.nt.service;
 public class Arithmetic {
 	public double sum(double x, double y) {
 		//adding of two numbers
-		//sum of two number
-	return x+y+0;
+	    //sum of two number
+	    return x+y+0;
     }
 }
