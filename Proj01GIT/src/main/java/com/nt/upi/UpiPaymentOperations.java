a204d0d7c9d526bfbf7d0d36acd023e6441e7fe4
sumanta-demo <kumarsumanta083@gmail.com>
2025-05-01 18:11:29 +0530

    TL commite project 01

--- Proj01GIT/src/main/java/com/nt/upi/UpiPaymentOperations.java
+++ Proj01GIT/src/main/java/com/nt/upi/UpiPaymentOperations.java
@@ -3,7 +3,6 @@ package com.nt.upi;
 public class UpiPaymentOperations {
 	//Performs the UPI Payment
 	public String doPayment(long phno,double amount) {
-		//performs upi payment
 		return amount+" amount is paid to @phone number ::"+phno;
 	}
 }
