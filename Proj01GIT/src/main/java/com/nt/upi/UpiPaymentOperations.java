6c5c3ada49abf2f97f62fc4ff1148a0113904dda
sumanta-demo <kumarsumanta083@gmail.com>
2025-05-01 16:34:29 +0530

    TL-Revied-b1 code

--- Proj01GIT/src/main/java/com/nt/upi/UpiPaymentOperations.java
+++ Proj01GIT/src/main/java/com/nt/upi/UpiPaymentOperations.java
@@ -1,7 +1,9 @@
 package com.nt.upi;
 
 public class UpiPaymentOperations {
+	//Performs the UPI Payment
 	public String doPayment(long phno,double amount) {
-		return amount+" amount is paid to phone number ::"+phno;
+		
+		return amount+" amount is paid to @phone number ::"+phno;
 	}
 }
