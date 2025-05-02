7b296f29fd1741beb8894238f05a03d0654827d3
sumanta-demo <kumarsumanta083@gmail.com>
2025-05-02 17:52:34 +0530

    TL shared project

---
+++ Proj02GIT-IgnoreFiles/src/main/java/nit/Proj02GIT_IgnoreFiles/App.java
@@ -0,0 +1,10 @@
+package nit.Proj02GIT_IgnoreFiles;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
